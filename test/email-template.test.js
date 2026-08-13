d1bb3a9b3d74be6f6192f6c5b902a72f37171f07
jblasing <jblasing@csllc-tx.com>
2026-08-13 14:02:49 -0500

    Test customer invitation email link and escaping

---
+++ test/email-template.test.js
@@ -0,0 +1,20 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const { invitationHtml } = require('../lib/email-templates');
+
+test('invitation email includes the private scheduling link and fallback URL', () => {
+  const url = 'https://example.com/book/private-token';
+  const html = invitationHtml('John', url);
+  assert.match(html, /Schedule Virtual Consultation/);
+  assert.equal(html.split(url).length - 1, 3);
+  assert.match(html, /45-minute virtual consultation/);
+  assert.match(html, /reschedule or cancel/);
+});
+
+test('invitation email escapes customer names and URLs', () => {
+  const html = invitationHtml('<John & Co>', 'https://example.com/?a=1&b=2');
+  assert.doesNotMatch(html, /<John/);
+  assert.match(html, /&lt;John &amp; Co&gt;/);
+  assert.match(html, /a=1&amp;b=2/);
+});
+
