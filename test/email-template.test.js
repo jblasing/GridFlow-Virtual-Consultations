395feb91a987c859522d8727ba9a00f5bf7ad289
jblasing <jblasing@csllc-tx.com>
2026-08-13 14:26:01 -0500

    Update invitation branding tests

--- test/email-template.test.js
+++ test/email-template.test.js
@@ -1,14 +1,15 @@
 const test = require('node:test');
 const assert = require('node:assert/strict');
-const { invitationHtml } = require('../lib/email-templates');
+const { invitationHtml, LOGO_URL } = require('../lib/email-templates');
 
 test('invitation email includes the private scheduling link and fallback URL', () => {
   const url = 'https://example.com/book/private-token';
   const html = invitationHtml('John', url);
-  assert.match(html, /Schedule Virtual Consultation/);
+  assert.match(html, /Schedule My Virtual Estimate/);
   assert.equal(html.split(url).length - 1, 3);
-  assert.match(html, /45-minute virtual consultation/);
+  assert.match(html, /45-minute virtual generator estimate/);
   assert.match(html, /reschedule or cancel/);
+  assert.match(html, new RegExp(LOGO_URL.replace(/[.*+?^${}()|[\\]\\]/g, '\\  assert.match(html, /reschedule or cancel/);')));
 });
 
 test('invitation email escapes customer names and URLs', () => {
